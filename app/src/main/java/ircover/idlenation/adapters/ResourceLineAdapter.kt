42f0d42dd19e583f75077c6f31002f55a2b11bab
Ircover <Ircover@ramble.rru>
2019-05-01 20:56:16 +0400

    refactor #5: выносит работу с боковой панелью из адаптера

--- app/src/main/java/ircover/idlenation/adapters/ResourceLineAdapter.kt
+++ app/src/main/java/ircover/idlenation/adapters/ResourceLineAdapter.kt
@@ -1,17 +1,13 @@
 package ircover.idlenation.adapters
 
-import android.content.Context
 import android.databinding.ObservableField
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
 import ircover.idlenation.R
 import ircover.idlenation.databinding.ListElementWorkPlaceBinding
 import ircover.idlenation.game.CountChangeObservable
 import ircover.idlenation.game.registerCountChangeListener
 import ircover.idlenation.utils.BindingViewHolder
-import ircover.idlenation.utils.commonFunctions.animateWidthChangeWithFading
-import ircover.idlenation.utils.commonFunctions.getRealWidth
 import ircover.idlenation.toCommonString
 import ircover.idlenation.utils.Disposable
 import org.apfloat.Apfloat
@@ -35,8 +31,8 @@ class WorkPlaceModel(val name: String,
     }
 }
 
-class ResourceLineHolder(context: Context, parent: ViewGroup?) :
-        BindingViewHolder<ListElementWorkPlaceBinding>(context, R.layout.list_element_work_place, parent) {
+class ResourceLineHolder(parent: ViewGroup) :
+        BindingViewHolder<ListElementWorkPlaceBinding>(R.layout.list_element_work_place, parent) {
     fun setBinding(workPlace: WorkPlaceModel, position: Int, onSelect: (Int) -> Unit) {
         binding?.workPlace = workPlace
         binding?.onClick = Runnable { onSelect(position) }
@@ -45,32 +41,21 @@ class ResourceLineHolder(context: Context, parent: ViewGroup?) :
 
 private val PAYLOAD_BACKGROUND = Any()
 
-class ResourceLineAdapter(private val context: Context) : RecyclerView.Adapter<ResourceLineHolder>() {
-    private var items: Array<WorkPlaceModel> = arrayOf()
-    private var maxDetailsWidth = 0
-    var viewToShowDetails: View? = null
-    set(value) {
-        field = value
-        value?.getRealWidth { width ->
-            maxDetailsWidth = width
-            value.layoutParams.width = 1
-            value.alpha = 0f
-        }
-    }
-    private var selectedPosition = -1
+class ResourceLineAdapter : RecyclerView.Adapter<ResourceLineHolder>() {
     var onSelectListener: ((WorkPlaceModel) -> Unit)? = null
+    private var items: Array<WorkPlaceModel> = arrayOf()
+    private var selectedPosition = -1
     private val onElementSelect: (Int) -> Unit = {
         if(selectedPosition >= 0) {
             notifyItemChanged(selectedPosition, PAYLOAD_BACKGROUND)
         }
         selectedPosition = it
         notifyItemChanged(selectedPosition, PAYLOAD_BACKGROUND)
-        viewToShowDetails?.animateWidthChangeWithFading(maxDetailsWidth, isFading = false)
         onSelectListener?.invoke(items[selectedPosition])
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResourceLineHolder =
-            ResourceLineHolder(context, parent)
+            ResourceLineHolder(parent)
 
     override fun getItemCount(): Int = items.size
 
@@ -87,20 +72,19 @@ class ResourceLineAdapter(private val context: Context) : RecyclerView.Adapter<R
         holder.refreshBackground(position)
     }
 
-    private fun ResourceLineHolder.refreshBackground(position: Int) {
-        itemView.setBackgroundResource(if(position != selectedPosition)
-            R.color.Transparent else R.color.SelectedElementBackground)
-    }
-
     fun setItems(workPlaces: Array<WorkPlaceModel>) {
         items = workPlaces
         notifyDataSetChanged()
     }
 
-    fun closeDetailsView() {
+    fun clearSelection() {
         val lastSelected = selectedPosition
         selectedPosition = -1
         notifyItemChanged(lastSelected, PAYLOAD_BACKGROUND)
-        viewToShowDetails?.animateWidthChangeWithFading(1, isFading = true)
+    }
+
+    private fun ResourceLineHolder.refreshBackground(position: Int) {
+        itemView.setBackgroundResource(if(position != selectedPosition)
+            R.color.Transparent else R.color.SelectedElementBackground)
     }
 }
